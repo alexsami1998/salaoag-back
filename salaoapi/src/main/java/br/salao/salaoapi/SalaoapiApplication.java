5a512244c45917d3566d1e73828058c2aeb72453
alexsami1998 <alexandre-enter86@live.com>
2023-10-20 20:25:09 -0300

    First commit | main class implements. And other configs how add updates of pom.xml

---
+++ salaoapi/src/main/java/br/salao/salaoapi/SalaoapiApplication.java
@@ -0,0 +1,13 @@
+package br.salao.salaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoapiApplication.class, args);
+	}
+
+}
---
+++ salaoapi/src/test/java/br/salaoapi/salao/SalaoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.salaoapi.salao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
