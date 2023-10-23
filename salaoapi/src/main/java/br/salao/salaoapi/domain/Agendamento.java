89c1f191c2389ecba56d4f128b2534196800e226
alexsami1998 <alexandre-enter86@live.com>
2023-10-23 20:53:15 -0300

    adjustment in scheduling.

--- salaoapi/src/main/java/br/salao/salaoapi/domain/Agendamento.java
+++ salaoapi/src/main/java/br/salao/salaoapi/domain/Agendamento.java
@@ -3,13 +3,20 @@ package br.salao.salaoapi.domain;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class Servico implements Serializable {
+import br.salao.salaoapi.domain.enums.Horarios;
+import br.salao.salaoapi.domain.enums.ServicoTipo;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Agendamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -22,9 +29,54 @@ public class Servico implements Serializable {
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
 	
-	private String nome;
-	private String descricao;
-	private double preco;
+	private String nomeCliente;
+	private String diaSemana;
+	private ServicoTipo servicoTipo;
+	private Horarios horarios;
+	private String observacao;
 	
-	//implementar o resto dos atributos e linkar as classes de agendamentos | criar todas as classes e logica de agendamento
+	/*@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;*/  //ajuste para automatizar direcionamento automatico de nomenclatura no front.
+	
+	public Agendamento() {
+		super();
+	}
+
+	public Agendamento(Integer id, String nomeCliente, String diaSemana, ServicoTipo servicoTipo, Horarios horarios,
+			String observacao) {
+		super();
+		this.id = id;
+		this.nomeCliente = nomeCliente;
+		this.diaSemana = diaSemana;
+		this.servicoTipo = servicoTipo;
+		this.horarios = horarios;
+		this.observacao = observacao;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agendamento other = (Agendamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
